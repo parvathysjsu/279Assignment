b2e55b982bf1502c78072927617dd0eff415ccbe
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-04-20 18:43:18 -0700

    server updated

--- assignment2/serverChild.c
+++ assignment2/serverChild.c
@@ -11,11 +11,28 @@
 int main(int argc, char const *argv[]) 
 { 
     printf("I am serverchild.c called by execvp() ");
+     //setuid
+        //nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin
+        int s = setuid(65534);
+        printf("setuid result is: %d\n",s );
+        printf("Real user id = %d, Effective User id = %d\n",getuid(),geteuid());
+        if(s ==-1){
+            perror(" Error with setuid() - errno " + errno);
+        }
     printf("\n");
     printf("argument:  %s",argv[1]);
     printf("\n");
     int fd = atoi(argv[1]);
     printf("int: %d",fd);
     printf("\n");
+         char buffer[1024] = {0}; 
+          char *hello = "Hello from server child"; 
+        //then read from client and print
+        int valread = read( fd , buffer, 1024); 
+        printf("%s\n",buffer ); 
+        send(fd , hello , strlen(hello) , 0 ); 
+        printf("Hello message sent\n"); 
+        printf("Real user id = %d, Effective User id = %d\n",getuid(),geteuid());
+
      return 0; 
 }
