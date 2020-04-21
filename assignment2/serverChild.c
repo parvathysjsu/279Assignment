e5b7a0c28ce7735234b366e5fb0830af06efc922
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-04-20 18:00:13 -0700

    assignment2

---
+++ assignment2/serverChild.c
@@ -0,0 +1,14 @@
+// Server side C/C++ program to demonstrate Socket programming 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <errno.h>
+#define PORT 80 
+
+int main(int argc, char const *argv[]) 
+{ 
+     return 0; 
+}
